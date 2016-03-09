7c744d5807d3213b261a637a9520a7356de42b13
Minute PHP <dev@minutephp.com>
2016-03-09 14:06:16 +0530

    minor fixes

--- src/js/angular-tags.js
+++ src/js/angular-tags.js
@@ -3,21 +3,35 @@
         .service('$replacetags', ['$session', function ($session) {
             var serviceInstance = {};
 
-            serviceInstance.replaceTags = function (text, replacements) {
-                replacements = replacements || {};
+            serviceInstance.replaceTags = function (text, hash) {
+                var replacements = {};
 
-                var replace = function (tag, hash) {
+                function flatten(json, flattened, str_key) {
+                    for (var key in json) {
+                        if (json.hasOwnProperty(key)) {
+                            if (json[key] instanceof Date) {
+                                flattened[str_key + (str_key ? "." : '') + key] = json[key] + '';
+                            } else if (json[key] instanceof Object && json[key] != "") {
+                                flatten(json[key], flattened, str_key + (str_key ? "." : '') + key);
+                            } else {
+                                flattened[str_key + (str_key ? "." : '') + key] = json[key] + '';
+                            }
+                        }
+                    }
+                }
+
+                flatten(angular.extend({}, hash, {session: $session}), replacements, "");
+
+                return (text || '').replace(/%([^%]+)%/g, function (all, tag) {
                     var parts = (tag || '').split('|');
-                    return ((hash || {})[parts[0]] || parts[1]) || '';
-                };
+                    var prefixes = (parts[0] || '').split('&');
+                    var key = prefixes.length > 1 ? prefixes[1] : prefixes[0];
+                    var prefix = prefixes.length > 1 ? prefixes[0] : '';
+                    var suffix = prefixes.length > 2 ? prefixes[2] : '';
+                    var value = (replacements || {})[key];
 
-                var obj = typeof($session) !== 'undefined' ? angular.extend({}, $session.site, $session.user, $session.request, replacements) : replacements || {};
-                var html = (text || '')
-                    .replace(/%([\w\-\|]+)%/g, function (all, key) { return replace(key, obj);})
-                    .replace(/%(\w+)\.([\w\-\|]+)%/g, function (all, key1, key2) {return replace(key2, obj[key1]);});
-
-
-                return html;
+                    return (value ? prefix + value + suffix : (parts[1] || ''));
+                });
             };
 
             return serviceInstance;
